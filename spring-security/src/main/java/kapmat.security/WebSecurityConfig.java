ac19cc4bb7602929fd001549aeb57a0a319b006f
Mateusz Kaproń <matkapron@gmail.com>
2016-09-11 18:35:41 +0200

    Spring-security module is working.

---
+++ spring-security/src/main/java/kapmat.security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Kapmat on 2016-09-10.
+ */
+package kapmat.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+				.authorizeRequests()
+					.antMatchers("/", "/home")
+					.permitAll()
+					.anyRequest()
+					.authenticated()
+					.and()
+				.formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.and()
+				.logout()
+					.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder authenticationBuilder) throws Exception {
+		authenticationBuilder
+				.inMemoryAuthentication()
+					.withUser("user")
+					.password("password")
+					.roles("USER");
+	}
+}
